969f9c3ab5f1e898e2226546c2a0b0410fd3ef74
amelie <ameliegitti@gmail.com>
2023-06-07 10:09:59 +0100

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded with ❤️ by Amelie Gitti and is {""}
+        <a href="https://github.com/amegi73/react-weather-app" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
