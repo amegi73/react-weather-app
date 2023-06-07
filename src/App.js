50ec284170591a93286b08a4f6d5650e99c6f03d
amelie <ameliegitti@gmail.com>
2023-06-07 11:10:02 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded with ❤️ by Amelie Gitti and is {""}
-        <a
-          href="https://github.com/amegi73/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded with ❤️ by Amelie Gitti and is {""}
+          <a
+            href="https://github.com/amegi73/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
