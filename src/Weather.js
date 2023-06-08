65ee0afd9361de187fa37dcc906e553adfbd22f7
amelie <ameliegitti@gmail.com>
2023-06-08 10:08:05 +0100

    added the date on the weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -14,7 +15,7 @@ export default function Weather(props) {
       iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png`,
       wind: response.data.wind.speed,
       city: response.data.city,
-      date: "Thursday 09:20",
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -42,7 +43,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
