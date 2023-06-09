56c4a0076fe3bc88f084a70a6fcc56b615a231a8
amelie <ameliegitti@gmail.com>
2023-06-09 09:52:18 +0100

    created component weatherTemperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,16 +15,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              className="float-left"
-            />
-
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
+            <img src={props.data.icon} alt={props.data.description} />
+            <span className="temp">
+              <WeatherTemperature celsius={props.data.temperature} />
             </span>
-            <span className="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
